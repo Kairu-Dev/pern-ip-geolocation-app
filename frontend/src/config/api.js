eac68c2caebcb20e7c9a9987112530f7d619e385
Kairu-Dev <kkairu.dev0.aph.odyssey@gmail.com>
2025-10-19 15:44:20 +0800

    Update 3.2
    Change API_BASE_URL TO Use the vercel app domain. To fix the can't found error, hopefully

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -3,4 +3,4 @@ const isDevelopment = import.meta.env.DEV;
 
 export const API_BASE_URL = isDevelopment
   ? 'http://localhost:8000/api'
-  : 'https://pern-ip-geolocation-app.vercel.app/api';
+  : 'https://pern-ip-geolocation-app.vercel.app/';
